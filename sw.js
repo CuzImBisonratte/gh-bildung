5878778fa664bc0ca5c6544b1fe2de642fde962b
Konstantin Protzen <cuzimbisonratte@gmail.com>
2023-05-14 22:05:26 +0200

    Fix scope and add pseudo-folders and notes

--- sw.js
+++ sw.js
@@ -3,7 +3,9 @@ const contentToCache = [
     '/res/fontawesome/css/solid.min.css',
     '/res/css/fonts.css',
     '/res/css/main.css',
+    '/app',
     '/app/',
+    '/app/assign',
     '/app/assign/',
     '/app/assign/index.html',
     '/app/assign/trainer.css',
@@ -13,19 +15,39 @@ const contentToCache = [
     '/app/intervals/script.js',
     '/app/intervals/style.css',
     '/app/intervals/trainer.css',
+    '/app/intervals/easy',
     '/app/intervals/easy/',
     '/app/intervals/easy/index.html',
     '/app/intervals/easy/intervals.js',
+    '/app/intervals/hard',
     '/app/intervals/hard/',
     '/app/intervals/hard/index.html',
     '/app/intervals/hard/intervals.js',
+    '/app/tonality',
     '/app/tonality/',
     '/app/tonality/index.html',
     '/app/tonality/script.js',
     '/app/tonality/style.css',
+    '/notes/c.wav',
+    '/notes/cs.wav',
+    '/notes/d.wav',
+    '/notes/ds.wav',
+    '/notes/e.wav',
+    '/notes/f.wav',
+    '/notes/fs.wav',
+    '/notes/g.wav',
+    '/notes/gs.wav',
+    '/notes/a.wav',
+    '/notes/as.wav',
+    '/notes/h.wav',
+    '/notes/c2.wav',
+    '/notes/cs2.wav',
+    '/notes/d2.wav',
+    '/notes/ds2.wav',
+    '/notes/e2.wav'
 ];
 
-const cacheName = "gh-bildung-v0-0-1";
+const cacheName = "gh-bildung-v0-0-1-aRandomName";
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
